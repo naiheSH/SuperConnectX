3563131e6ce72f823d0cfaad22b7dd24ca86bd0f
superstudio <secret>
2026-06-07 14:47:02 +0800

    优化颜色选择对话框显示

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { i18n } from './locales'
@@ -10,4 +11,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus).use(i18n).mount('#app')
+app.use(ElementPlus, { locale: zhCn }).use(i18n).mount('#app')
