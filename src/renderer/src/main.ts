909a229a05babe5eba5fd5618e3eec81f4d9ff2a
superstudio <secret>
2026-06-21 23:07:10 +0800

    连接失败的提示显示的太久了, 老是挡住我要点的界面

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -5,6 +5,10 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { i18n } from './locales'
+import { messageConfig } from 'element-plus/es/components/config-provider/src/config-provider'
+
+// 全局设置 ElMessage 默认显示时长 1.5 秒（默认 3 秒太久了）
+messageConfig.duration = 1000
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
